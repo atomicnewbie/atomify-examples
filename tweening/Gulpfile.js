3a8d548594c5458fe04871847dbb9cdb59b77550
atomicnewbie <atomicnewbie@users.noreply.github.com>
2015-07-03 19:42:09 -0700

    Add clean task. Copy output to build directory.

--- tweening/Gulpfile.js
+++ tweening/Gulpfile.js
@@ -2,8 +2,13 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var del = require('del');
 
-gulp.task('default', function () {
+gulp.task('clean', function(cb) {
+  del(['build'], cb);
+});
+
+gulp.task('atomify', function () {
   // set up the browserify instance on a task basis
   var b = browserify({
      entries: './app.js',
@@ -13,5 +18,12 @@ gulp.task('default', function () {
   return b.bundle()
     .pipe(source('main.js'))
     .pipe(buffer())
-    .pipe(gulp.dest('./atomic/Resources/Scripts'));
+    .pipe(gulp.dest('./build/Resources/Scripts'));
 });
+
+gulp.task('copy-files', function() {
+  return gulp.src(['atomic/**/*']).pipe(gulp.dest('build'));
+});
+
+
+gulp.task('default', ['copy-files', 'atomify']);
